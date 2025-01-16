2a5d3bfc5b854f61571b1ab01af31a58605f6cb9
9948180189 <hari10.143@gmail.com>
2025-01-16 10:36:37 +0530

    adding code to backend server

---
+++ backend.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+LOGS_FLODER="/var/log/expense"
+SCRIPT_NAME=$(echo "16-redirectors.sh" | cut -d "." -f1)
+TIMESTAMP=$(date +%Y-%m-%d-%H-%M-%S)
+LOG_FILE="$LOG_FOLDER/$SCRIPT_NAME-$TIMESTAMP.log"
+mkdir -p $LOGS_FLODER
+USERID=$(id -u)
+R="\e[31m"
+G="\e[32m"
+N="\e[0m"
+Y="\e[33m"
+
+CHECK_ROOT(){
+    if [ $USERID -ne 0 ] 
+    then
+        echo -e "$R Please run this script with root priveleges $N" | tee -a $LOG_FILE
+        exit 1
+    fi
+}
+
+VALIDATE(){
+    if [ $1 -ne 0 ]
+    then
+        echo -e "$2 is...$R FAILED $N" | tee -a $LOG_FILE
+        exit 1
+    else
+        echo -e "$2 is...$G success $N" | tee -a $LOG_FILE
+    fi
+}
+echo "script started executing at: $(date)" | tee -a $LOG_FILE
+CHECK_ROOT
+
+dnf module disable nodejs -y
+VALIDATE $? "Diasable defaults nodejs"
+
+dnf module enable nodejs:20 -y
+VALIDATE $? "Enabled nodejs:20"
+
+dnf install nodejs -y
+VALIDATE $? "Install nodejs:20"
+
+useradd expense
+VALIDATE $? "creating expense user"
+
